5f83b2762413847c79f584b80c5c4ea1e5ee8d78
Ryan Romanchuk <rromanchuk@gmail.com>
2017-12-22 23:17:58 -0600

    add additional slaves

--- start.sh
+++ start.sh
@@ -32,9 +32,11 @@
 
 sudo systemctl start crawl
 sudo systemctl start crawl_slave
+sudo systemctl start crawl_slave2
 
-sudo systemctl start ping_slave
 sudo systemctl start ping
+sudo systemctl start ping_slave2
+sudo systemctl start ping_slave
 
 sudo systemctl start resolve
 sudo systemctl start export
